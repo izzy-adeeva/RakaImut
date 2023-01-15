353066b7789af0ac16b7126d1db0a2eb71435a05
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-16 02:01:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 FORCE_SUB_GROUP2 = int(os.environ.get("FORCE_SUB_GROUP2", "0"))
-
+FORCE_SUB_GROUP3 = int(os.environ.get("FORCE_SUB_GROUP3", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 # Pesan Awalan /start
